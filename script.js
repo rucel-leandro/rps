8c35928a5bc2e79c20fd519251e0c619b519f727
rucel-leandro <80652683+rucel-leandro@users.noreply.github.com>
2024-02-28 17:57:55 +0100

    getComputerChoice functionn works

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-console.log("Hello Mundo!")
+// Functions
 
-console.log("Ciao Bella Test");
+function getComputerChoise() {
+    let randomPick = Math.random(); 
+    console.log(randomPick);
+
+   
+    if (randomPick <= 0.33) {
+        console.log("Rock"); 
+    } else if (randomPick >= 0.33 && randomPick <= 0.66) { 
+        console.log("Paper"); 
+    } else if (randomPick >= 0.66 && randomPick <= 1) { 
+        console.log("Scissors"); } 
+        else console.log("Something's wrong!")
+
+}
+
