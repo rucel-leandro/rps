46f248b5d2cfa959c04a65c3ad3e76172afdc92e
rucel-leandro <80652683+rucel-leandro@users.noreply.github.com>
2024-03-02 19:09:52 +0100

    remade game logic as a function instead of if statements

--- script.js
+++ script.js
@@ -15,45 +15,55 @@ function getComputerChoise() {
 
 }
 
-let computerSelection = getComputerChoise(); 
-console.log(computerSelection);
-
-let playersSelection = prompt("Make a choice")
+let computerSelection = getComputerChoise();
 
 
-// Game logic not working. If it is a draw it outputs You lose!
+
+// Game logic not working. If it is a draw it outputs You lose! Check winning condition for the Computer
     
-    if (playersSelection == computerSelection) {
-        console.log("It's a draw!"); 
-    } switch (playersSelection , computerSelection) {
-        case "Rock", "Paper":
-            console.log(`You lose! ${computerSelection} beats ${playersSelection}`);
-            break;
-        case "Rock", "Scissors":
-            console.log(`You win! ${playersSelection} beats ${computerSelection}`);
-            break;
+   
+// if (playersSelection === computerSelection) {
+//     console.log("It's a draw!");
+// } switch (playersSelection , computerSelection) {
+//         case ("Rock", "Paper"):
+//             console.log(`You lose! ${computerSelection} beats ${playersSelection}`);
+//             break;
+//         case ("Rock", "Scissors"):
+//             console.log(`You win! ${playersSelection} beats ${computerSelection}`);
+//             break;
 
-        case "Paper", "Rock":
-            console.log(`You win! ${playersSelection} beats ${computerSelection}`);
-            break;
+//         case ("Paper", "Rock"):
+//             console.log(`You win! ${playersSelection} beats ${computerSelection}`);
+//             break;
 
-        case "Paper", "Scissors":
-          console.log(`You lose! ${computerSelection} beats ${playersSelection}`);
-            break;
+//         case ("Paper", "Scissors"):
+//           console.log(`You lose! ${computerSelection} beats ${playersSelection}`);
+//             break;
 
-        case "Scissors", "Paper":
-        console.log(`You win! ${playersSelection} beats ${computerSelection}`);
-        break;
+//         case ("Scissors", "Paper"):
+//         console.log(`You win! ${playersSelection} beats ${computerSelection}`);
+//         break;
 
-        case "Scissors", "Rock":
-        console.log(`You lose! ${computerSelection} beats ${playersSelection}`);
-        break;
-        
-        default: 
-        console.log("Error"); 
+//         case ("Scissors", "Rock"):
+//         console.log(`You lose! ${computerSelection} beats ${playersSelection}`);
+//         break;
+
+//         default: 
+//         console.log("Error"); 
               
+//     }
+    
+
+
+function playGame (playersSelection , computerSelection = getComputerChoise()) {   
+    console.log(computerSelection);
+    console.log(playersSelection);
+
+    if (playersSelection === computerSelection) {
+        console.log("It's a draw!")
     }
     
 
-
+  
+}
 
